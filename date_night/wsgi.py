4db68128946c6298bcd93ea103466e990c1c9745
Niki Macomber <nikicrisler@gmail.com>
2014-09-22 13:20:27 -0700

    fixed wsgi

--- date_night/wsgi.py
+++ date_night/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "date_night.settings")
 
-application = get_wsgi_application()
+
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
